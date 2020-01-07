b9e8c665b15e0e5aa4b41b266f9414bd4dc4cb8a
Tina Hang <mstinahang@gmail.com>
2020-01-07 15:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-animal = ""
+const animal = "dog";
 
 function myAnimal() {
-  let animal = animal
   return animal
 }
 
@@ -9,6 +8,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  let animal = 'cat';
   return animal
 }
 
